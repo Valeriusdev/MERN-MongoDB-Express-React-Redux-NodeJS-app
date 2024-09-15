87c8425187a7dee3cdf61a4dfe6588d68ff48761
Valeriusdev <kakao757@gmail.com>
2024-09-14 17:55:37 -0700

    add react router

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from "react-router-bootstrap";
 
 
 const Header = () => {
@@ -7,12 +8,18 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'> MERN Cake Shop </Navbar.Brand>
+              <LinkContainer to="/">
+                <Navbar.Brand> MERN Cake Shop </Navbar.Brand>
+              </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                   <Nav className='ms-auto'>
-                    <Nav.Link href='/cart'> <FaShoppingCart /> Cart </Nav.Link>
-                    <Nav.Link href='/login'> <FaUser /> Sign In </Nav.Link>
+                    <LinkContainer to='/cart'>
+                      <Nav.Link> <FaShoppingCart /> Cart </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/login'>
+                      <Nav.Link> <FaUser /> Sign In </Nav.Link>
+                    </LinkContainer> 
                   </Nav>
                 </Navbar.Collapse>
             </Container>
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter (
+  createRoutesFromElements (
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
