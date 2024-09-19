8b81d9ed05c9bca43bd38c72e23fcc14326fe395
Valeriusdev <kakao757@gmail.com>
2024-09-18 23:37:13 -0700

    add server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
