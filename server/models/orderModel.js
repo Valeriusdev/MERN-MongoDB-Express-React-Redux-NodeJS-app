60569cfccad7c375cb28247148fbda1aa7579596
Valeriusdev <kakao757@gmail.com>
2024-09-26 14:39:03 -0700

    add seeder.js

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -75,6 +75,6 @@ const orderSchema = mongoose.Schema({
     timestamps: true,
 });
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
 export default Order;
