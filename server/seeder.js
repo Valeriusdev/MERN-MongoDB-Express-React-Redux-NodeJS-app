94bea21c12fdab808ae739122c4dd81267e096f8
Valeriusdev <kakao757@gmail.com>
2024-09-26 21:18:26 -0700

    update seeder.js

--- server/seeder.js
+++ server/seeder.js
@@ -18,7 +18,7 @@ const importData = async () => {
         await Product.deleteMany();
         await User.deleteMany();
 
-        const createUsers = await User.insertMany(users);
+        const createdUsers = await User.insertMany(users);
         const adminUser = createdUsers[0]._id;
 
         const sampleProducts = products.map((product) => {
