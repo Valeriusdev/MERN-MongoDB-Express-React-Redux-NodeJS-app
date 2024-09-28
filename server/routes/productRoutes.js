b57dadfa7b35ca01cdf204a31f05381a9599df28
Valeriusdev <kakao757@gmail.com>
2024-09-27 20:51:56 -0700

    add asyncHandler.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
 const router = express.Router();
-import products from "../data/products.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
-
-router.get('/', (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
-});
+}));
 
-router.get('/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
     res.json(product);
-});
+}));
 
 export default router;
