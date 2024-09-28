ea905af0236d2119ee91d39dbc978557fd5890cf
Valeriusdev <kakao757@gmail.com>
2024-09-27 21:28:11 -0700

    update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -10,7 +10,12 @@ router.get('/', asyncHandler(async (req, res) => {
 
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-    res.json(product);
+
+    if (product) {
+        return res.json(product);
+    }
+
+    res.status(404).json({ message: 'Product not found'})
 }));
 
 export default router;
