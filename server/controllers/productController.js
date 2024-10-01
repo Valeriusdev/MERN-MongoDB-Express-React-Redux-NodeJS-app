cdb48ee1c3db464ba23955fd56f57fceb58d6f60
Valeriusdev <kakao757@gmail.com>
2024-09-30 23:08:48 -0700

    update productController.js

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,11 +1,17 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
 });
 
+// @desc    Fetch a product
+// @route   GET /api/products/:id
+// @access  Public
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
