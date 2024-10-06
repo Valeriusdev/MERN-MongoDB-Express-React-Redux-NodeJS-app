0c860bb708ffa6ae38847a0d22a231f6291875e3
Valeriusdev <kakao757@gmail.com>
2024-10-05 20:31:18 -0700

    update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,4 +2,4 @@ export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localho
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/paypal';
+export const PAYPAL_URL = '/api/config/paypal';
