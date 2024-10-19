033944118387cb768a5eb201f6a82c01ea2d0f9a
Valeriusdev <kakao757@gmail.com>
2024-10-18 21:32:48 -0700

    update ProductScreen component

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 
