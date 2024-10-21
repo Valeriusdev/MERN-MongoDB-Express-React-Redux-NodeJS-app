73ede362852cedfb0d6e446dafd455303ee13fd9
Valeriusdev <kakao757@gmail.com>
2024-10-20 23:43:01 -0700

    update HomeScreen component

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
@@ -10,7 +11,7 @@ const HomeScreen = () => {
         <>
             { isLoading ? (
                 <Loader />
-            ) : error ? (<div>{ error?.data?.message || error.error }</div>) : (<>
+            ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
                 <h1>Latest Products</h1>
                 <Row>
                     {products.map((product)=> (
