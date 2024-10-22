cfdd9f82361f514fdc3a3cfc4a4be08ea7ed6130
Valeriusdev <kakao757@gmail.com>
2024-10-21 20:06:16 -0700

    update ProductScreen component

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
+import Message from '../components/Message';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 
@@ -18,7 +19,7 @@ const ProductScreen = () => {
             { isLoading ? (
                 <Loader />
             ) : error ? (
-                <div>{ error?.data?.message || error.error }</div>
+                <Message variant='danger'>{ error?.data?.message || error.error }</Message>
             ) : (
                 <Row>
                     <Col md={5}>
