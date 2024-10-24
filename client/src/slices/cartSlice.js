4df231e4c0b9c024504e90e205a6a3f3fbd5c998
Valeriusdev <kakao757@gmail.com>
2024-10-23 20:26:58 -0700

    update cartSlice.js

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -9,7 +9,16 @@ const cartSlice = createSlice({
     reducers: {
         addtoCart: (state, action) => {
             const item = action.payload;
+
             const existItem = state.cartItems.find((x) => x._id === item._id);
+        
+
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) => x._id === 
+                existItem._id ? item : x );
+            } else {
+                state.cartItems = [...state.cartItems, item];
+            }
         }
     },
 });
