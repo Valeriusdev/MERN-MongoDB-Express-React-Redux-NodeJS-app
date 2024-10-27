6ed8a8767e09cd1e200a40e93894f4d5fca28c4b
Valeriusdev <kakao757@gmail.com>
2024-10-26 22:36:50 -0700

    update cartSlice.js

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -33,6 +33,12 @@ const cartSlice = createSlice({
 
             //Calculate tax price (15% tax)
             state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+            state.totalPrice =(
+                Number(state.itemsPrice) +
+                Number(state.shippingPrice) +
+                Number(state.taxPrice)
+            ).toFixed(2);
         }
     },
 });
