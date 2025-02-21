9030fdbf86c09e54fa741d7017090d14f49473f1
Valeriusdev <kakao757@gmail.com>
2025-02-21 03:26:45 -0800

    update server.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,19 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
